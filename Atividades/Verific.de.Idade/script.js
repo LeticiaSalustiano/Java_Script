578d3cf1d3667201e7228247f9ca2d843bc6401b
LeticiaSalustiano <leticiasalustiano07@gmail.com>
2024-09-12 08:53:07 -0300

    Finalizado

--- Atividades/Verific.de.Idade/script.js
+++ Atividades/Verific.de.Idade/script.js
@@ -20,35 +20,35 @@ function verificar(){
     switch(genero){
         case 'M':
             if(idade > 124 || idade < 1){
-                texto.innerText = 'ERRO - Digite uma data válida (De 1900 à 2023)'
-                window.alert('[ERRO] - Digite uma data válida (De 1900 à 2023)')
+                texto.innerText = 'ERRO - Digite uma data válida (De 1900 à 2024)'
+                window.alert('[ERRO] - Digite uma data válida (De 1900 à 2024)')
                 img.style.display = 'none'
             } else if(idade <= 2){
-                texto.innerText = `Você é um bebê de ${idade} anos!`
+                texto.innerText = `Você é um bebê de ${idade} anos`
                 img.style.display = 'block'
                 img.style.margin = 'auto'
                 img.style.paddingBottom = '10px'
                 img.src = 'imagens/bebemenino.png'
             } else if(idade < 14){
-                texto.innerText = `Você é uma criança de ${idade} anos!`
+                texto.innerText = `Você é uma criança de ${idade} anos`
                 img.style.display = 'block'
                 img.style.margin = 'auto'
                 img.style.paddingBottom = '10px'
                 img.src = 'imagens/criancamenino.webp' 
             } else if(idade <18){
-                texto.innerText = `Você é um adolescente de ${idade} anos!`
+                texto.innerText = `Você é um adolescente de ${idade} anos`
                 img.style.display = 'block'
                 img.style.margin = 'auto'
                 img.style.paddingBottom = '10px'
                 img.src = 'imagens/adolescentemasc.jpg' 
             } else if(idade < 65){
-                texto.innerText = `Você é um homem de ${idade} anos!`
+                texto.innerText = `Você é um homem de ${idade} anos`
                 img.style.display = 'block'
                 img.style.margin = 'auto'
                 img.style.paddingBottom = '10px'
                 img.src = 'imagens/homem.jpg'
             } else {
-                texto.innerText = `Você é um senhor de ${idade} anos!`
+                texto.innerText = `Você é um senhor de ${idade} anos`
                 img.style.display = 'block'
                 img.style.margin = 'auto'
                 img.style.paddingBottom = '10px'
@@ -58,36 +58,36 @@ function verificar(){
 
         case 'F':
             if(idade > 124 || idade < 1){
-                texto.innerText = 'ERRO - Digite uma data válida (De 1900 à 2023)'
-                window.alert('[ERRO] - Digite uma data válida (De 1900 à 2023)')
+                texto.innerText = 'ERRO - Digite uma data válida (De 1900 à 2024)'
+                window.alert('[ERRO] - Digite uma data válida (De 1900 à 2024)')
                 img.style.display = 'none'
             } else if(idade <= 2){
-                texto.innerText = `Você é uma bebê de ${idade} anos!`
+                texto.innerText = `Você é uma bebê de ${idade} anos`
                 img.style.display = 'block'
                 img.style.margin = 'auto'
                 img.style.paddingBottom = '10px'
                 img.src = 'imagens/bebemenina.jpg' 
                 
             } else if(idade < 14){
-                texto.innerText = `Você é uma criança de ${idade} anos!`
+                texto.innerText = `Você é uma criança de ${idade} anos`
                 img.style.display = 'block'
                 img.style.margin = 'auto'
                 img.style.paddingBottom = '10px'
                 img.src = 'imagens/criancamenina.jpg' 
             } else if(idade <18){
-                texto.innerText = `Você é uma adolescente de ${idade} anos!`
+                texto.innerText = `Você é uma adolescente de ${idade} anos`
                 img.style.display = 'block'
                 img.style.margin = 'auto'
                 img.style.paddingBottom = '10px'
                 img.src = 'imagens/adolescentefem.jpg' 
             } else if(idade < 65){
-                texto.innerText = `Você é uma mulher de ${idade} anos!`
+                texto.innerText = `Você é uma mulher de ${idade} anos`
                 img.style.display = 'block'
                 img.style.margin = 'auto'
                 img.style.paddingBottom = '10px'
                 img.src = 'imagens/mulher.jpg' 
             } else {
-                texto.innerText = `Você é um senhora de ${idade} anos!`
+                texto.innerText = `Você é um senhora de ${idade} anos`
                 img.style.display = 'block'
                 img.style.margin = 'auto'
                 img.style.paddingBottom = '10px'
